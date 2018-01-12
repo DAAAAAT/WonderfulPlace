b92f5c6d0eb110bb6adc2d43cb902ee43bed4036
badjok3 <dani.popoff@abv.bg>
2018-01-12 18:10:46 +0200

    merged Article and Destination models into Destination.js

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,7 +2,7 @@ const authCheck = require('../middleware/auth-check');
 const roleCheck = require('../middleware/role-check');
 
 const authRoutes = require('./auth');
-const profileRoutes = require('./profile')
+const profileRoutes = require('./profile');
 const about = require('./about');
 const homeRoutes = require('./home');
 const adminRoutes = require('./admin');
@@ -12,7 +12,7 @@ module.exports = (app) => {
     app.post('/login', authRoutes.login);
     app.post('/register', authRoutes.register);
     app.use('/profile', authCheck, profileRoutes);
-    app.use('/', homeRoutes);    
+    app.use('/', homeRoutes);
     app.use('/admin',  adminRoutes);
     app.use('/destination',  destinationRoutes);
     // app.get('/about',roleCheck('Admin'), about.getAbout);

24a7426183d119a61edae776688597b8e8df99ec
Cvetomir <fingerflip97@gmail.com>
2018-01-13 13:59:59 +0200

    Merge bug fix

--- front-end/src/app/core/models/view-models/destination.view-model.ts
+++ front-end/src/app/core/models/view-models/destination.view-model.ts
@@ -1,17 +1,15 @@
 import {CategoryViewModel} from './category.view-model';
 
 export class DestinationViewModel {
-  constructor(
-    public name: string,
-    public images: any[],
-    public latitude: number,
-    public longitude: number,
-    public city: string,
-    public country: string,
-    public description: string,
-    public author: string,
-    public comments: string,
-    public category: CategoryViewModel,
+    public name: string
+    public images: any[]
+    public latitude: number
+    public longitude: number
+    public city: string
+    public country: string
+    public description: string
+    public author: string
+    public comments: string
+    public category: CategoryViewModel
     public rating: number
-  ){ }
 }

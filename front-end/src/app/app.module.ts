fea4c7a71e0233834ae8ccbcb154b21a9d978d8d
Alisa Terzieva <alisa.terzieva@gmail.com>
2018-01-14 12:29:54 +0200

    Added infowindow to the map

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,25 +1,25 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {ReactiveFormsModule,  FormsModule} from '@angular/forms';
-import {AuthenticationModule} from './components/auth/auth.module';
-import {HomeComponent} from './components/home/home.component';
-import {routes} from './router';
-import {WishListComponent} from './components/wish-list/wish-list.component';
-import {SharedModule} from './components/shared/shared.module';
-import {DestinationModule} from './components/destinations/destination.module';
-import {HttpClientService} from './core/services/auth/http-client.service';
-import {TokenService} from './core/services/auth/token.service';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {AngularFontAwesomeModule} from "angular-font-awesome";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AuthenticationModule } from './components/auth/auth.module';
+import { HomeComponent } from './components/home/home.component';
+import { routes } from './router';
+import { WishListComponent } from './components/wish-list/wish-list.component';
+import { SharedModule } from './components/shared/shared.module';
+import { DestinationModule } from './components/destinations/destination.module';
+import { HttpClientService } from './core/services/auth/http-client.service';
+import { TokenService } from './core/services/auth/token.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFontAwesomeModule } from "angular-font-awesome";
 import { ProfileComponent } from './components/profile/profile.component';
-import {CreateCategoryComponent} from './components/categories/create-category/create-category.component';
+import { CreateCategoryComponent } from './components/categories/create-category/create-category.component';
 
 //Google Maps module
-import {AgmCoreModule} from '@agm/core'
-import {GuardsModule} from './core/guards/guards.module';
-import {RouterModule} from '@angular/router';
-import {environment} from '../environments/environment';
+import { AgmCoreModule } from '@agm/core'
+import { GuardsModule } from './core/guards/guards.module';
+import { RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
